5e0831dddebe4752d058442e6f492700bd9b4578
Kundan1257 <kk0555265@gmail.com>
2026-06-25 23:14:14 +0530

    style: remove visual vital tracker from layout configuration

--- src/App.tsx
+++ src/App.tsx
@@ -957,8 +957,7 @@ function AppContent() {
   } />
               <Route path="/exercises" element={<ExerciseSection />} />
               <Route path="/diet" element={<DietSection />} />
-              <Route path="/help" element={<div id="ex">              <TrackerSection />
-              <HelpSection /></div>} />
+              <Route path="/help" element={<div id="ex">                            <HelpSection /></div>} />
              
               <Route path="/checkout" element={<CheckoutSection />} />
               <Route path="/about" element={<AboutPage />} />
