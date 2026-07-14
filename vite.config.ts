29e79f0b76abb02de856d59de9b8fdd0b09c8531
Kundan1257 <kk0555265@gmail.com>
2026-07-14 10:52:29 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -26,20 +26,33 @@ export default defineConfig(({ mode }) => {
           background_color: '#ffffff',
           display: 'standalone',
           orientation: 'portrait',
-          icons: [
+                    icons: [
             {
               src: 'icon-192.png',
               sizes: '192x192',
               type: 'image/png',
-              purpose: 'any maskable'
+              purpose: 'any'
+            },
+            {
+              src: 'icon-192.png',
+              sizes: '192x192',
+              type: 'image/png',
+              purpose: 'maskable'
             },
             {
               src: 'icon-512.png',
               sizes: '512x512',
               type: 'image/png',
-              purpose: 'any maskable'
+              purpose: 'any'
+            },
+            {
+              src: 'icon-512.png',
+              sizes: '512x512',
+              type: 'image/png',
+              purpose: 'maskable'
             }
           ]
+
         }
       })
     ],
